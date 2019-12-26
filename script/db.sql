6a6a078aa569404090a6fdd526b7a99cfbd8120e
marshall <uxff@qq.com>
2019-12-26 15:02:57 +0800

    add group id

--- script/db.sql
+++ script/db.sql
@@ -1,16 +1,17 @@
 
 # 创建定时任务表
 CREATE TABLE `athena_cronjobs` (
-                            `id` int(11) NOT NULL AUTO_INCREMENT,
-                            `name` varchar(50) DEFAULT '',
-                            `url` varchar(256) NOT NULL DEFAULT '',
-                            `expression` varchar(100) NOT NULL DEFAULT '',
-                            `status` tinyint DEFAULT 1,
-                            `retries` int(11) DEFAULT '0',
-                            `request_timeout` int(11) DEFAULT '3',
-                            `stop_at` timestamp NOT NULL DEFAULT '1999-01-01 00:00:00',
-                            `created_at` timestamp NOT NULL DEFAULT '1999-01-01 00:00:00',
-                            `updated_at` timestamp NOT NULL DEFAULT '1999-01-01 00:00:00',
+                            `id` INT(11) NOT NULL AUTO_INCREMENT,
+                            `name` VARCHAR(50) DEFAULT '',
+                            `url` VARCHAR(256) NOT NULL DEFAULT '',
+                            `expression` VARCHAR(100) NOT NULL DEFAULT '',
+                            `status` TINYINT UNSIGNED NOT NULL DEFAULT 1,
+                            `retries` INT(11) UNSIGNED NOT NULL DEFAULT 0,
+                            `request_timeout` INT(11) UNSIGNED NOT NULL DEFAULT 3,
+                            `stop_at` TIMESTAMP NOT NULL DEFAULT '1999-01-01 00:00:00',
+                            `created_at` TIMESTAMP NOT NULL DEFAULT '1999-01-01 00:00:00',
+                            `updated_at` TIMESTAMP NOT NULL DEFAULT '1999-01-01 00:00:00',
+                            `group_id` INT(11) UNSIGNED NOT NULL DEFAULT '0',
                             PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8;
 
