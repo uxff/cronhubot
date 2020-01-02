7bb8619855d1123006b07fc55220dc4b261d3b28
marshall <uxff@qq.com>
2020-01-02 18:05:21 +0800

    update dist main

--- cmd/disthubot/main.go
+++ cmd/disthubot/main.go
@@ -11,16 +11,22 @@ import (
 	"flag"
 	"github.com/uxff/cronhubot/pkg/config"
 	"github.com/uxff/cronhubot/pkg/log"
+	"github.com/uxff/electio/worker"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	slog "log"
 	"os"
+	"strings"
+	"sync"
 )
 
 var (
 	version         = "0.1"
 	showVersion 	bool
 	logLevel     	= 0
+	clusterName     = "c1"
+	clusterMembers  = "127.0.0.1:9001,127.0.0.1:9002,127.0.0.1:9003"
+	clusterNodeAddr = "127.0.0.1:9001"
 )
 
 
@@ -28,6 +34,9 @@ func main() {
 
 	flag.IntVar(&logLevel, "l", logLevel, "log logLevel, -1:debug, 0:info, 1:warn, 2:error")
 	flag.BoolVar(&showVersion, "v", false, "show version")
+	flag.StringVar(&clusterName, "c", clusterName, "cluster name on it")
+	flag.StringVar(&clusterMembers, "m", clusterMembers, "cluster members")
+	flag.StringVar(&clusterNodeAddr, "a", clusterNodeAddr, "cluster node addr that you will listen as serve as a cluster node")
 	flag.Parse()
 
 	if showVersion {
@@ -51,7 +60,29 @@ func main() {
 		log.Fatalf("failed to load env:%v", err)
 	}
 
-	if err := Serve(env); err != nil {
+
+	workerNode := worker.NewWorker(clusterNodeAddr, clusterName)
+	workerNode.AddMates(strings.Split(clusterMembers, ","))
+
+	errCh := make(chan error, 1)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	// 启动cron服务
+	go func() {
+		errCh<-Serve(env)
+		wg.Done()
+	}()
+
+	// 启动分布式集群节点
+	go func() {
+		errCh <- workerNode.Start()
+		wg.Done()
+	}()
+
+	err = <-errCh
+	if err != nil {
 		log.Fatalf("main error:%v", err)
 	}
 }
